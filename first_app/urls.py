4b7b10708feec525162b373f0e36662fc23b8bae
Roshankc682 <kcroshan682@gmail.com>
2021-06-05 20:12:02 +0545

    __||__

--- first_app/urls.py
+++ first_app/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView
 urlpatterns = [
     path('register/',views.RegisterView.as_view(), name='Register_account'),
     path('api/token/', views.MyTokenObtainPairView.as_view(), name='Token_create'),
-    path('api/token/new/', views.user_new_access_and_refrsh_token_and.as_view(), name='Token_refresh_of_reflecting_acces_token'),
+    path('api/token/new/', views.user_new_access_and_refrsh_token_and, name='Token_refresh_of_reflecting_acces_token'),
     path('api/access/refresh/', TokenRefreshView.as_view(), name='Get_access_if_page_refresh'),
     path('api/logout', views.logout, name='Logout'),
-    path('api/dashboard/', views.get_data_of_user.as_view(), name='Token_create'),
+    path('api/dashboard/', views.get_data_of_user, name='Token_create'),
 ]
