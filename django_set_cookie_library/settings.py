77f137bc2f4f967b8a73b9f3673e9568e1a49b23
Roshankc682 <kcroshan682@gmail.com>
2021-06-05 17:25:07 +0545

    __||__

--- django_set_cookie_library/settings.py
+++ django_set_cookie_library/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework',
+    # 'rest_framework',
     'first_app'
 ]
 REST_FRAMEWORK = {
