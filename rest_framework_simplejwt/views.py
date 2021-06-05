b11a6159e18e654692a7fbdb50f100d68c8a12f5
Roshankc682 <kcroshan682@gmail.com>
2021-06-05 20:33:49 +0545

    __||__

--- rest_framework_simplejwt/views.py
+++ rest_framework_simplejwt/views.py
@@ -33,9 +33,10 @@ class TokenViewBase(generics.GenericAPIView):
             if request.build_absolute_uri() == "http://api-v1-backend.herokuapp.com/api/access/refresh/" \
                     or \
                     request.build_absolute_uri() == "http://localhost:8000/api/access/refresh/":
-                return Response({"details":"refresh the token","message": request.build_absolute_uri()}, status=status.HTTP_400_BAD_REQUEST)
+                pass
+                # return Response({"details":"refresh the token","message": request.build_absolute_uri()}, status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response({"details":"new token","message": request.build_absolute_uri()}, status=status.HTTP_400_BAD_REQUEST)
+                # return Response({"details":"new token","message": request.build_absolute_uri()}, status=status.HTTP_400_BAD_REQUEST)
                 if request.build_absolute_uri() == "http://api-v1-backend.herokuapp.com/api/token/" \
                         or \
                         request.build_absolute_uri() == "http://localhost:8000/api/token/":
@@ -51,6 +52,7 @@ class TokenViewBase(generics.GenericAPIView):
                                 url = f"https://www.google.com/recaptcha/api/siteverify?secret={secret}&response={user_data_dic['recapcha']}"
                                 x = requests.post(url)
                                 response_dict = json.loads(x.text)
+                                pass
                                 # if response_dict["success"] == True:
                                 #    pass
                                 # else:
