a7f2ca9e8b70a8fcd8f1a5e539ba7e3c1f2b0ebd
Roshankc682 <kcroshan682@gmail.com>
2021-06-05 17:11:53 +0545

    __||__

--- first_app/urls.py
+++ first_app/urls.py
@@ -6,7 +6,6 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView
 # make a env file and import all requirements
 urlpatterns = [
     path('register/',views.RegisterView.as_view(), name='Register_account'),
-    # path('api/token/', views.MyTokenObtainPairView.as_view(), name='Token_create'),
     path('api/token/', views.MyTokenObtainPairView.as_view(), name='Token_create'),
     path('api/token/new/', views.user_new_access_and_refrsh_token_and, name='Token_refresh_of_reflecting_acces_token'),
     path('api/access/refresh/', TokenRefreshView.as_view(), name='Get_access_if_page_refresh'),
